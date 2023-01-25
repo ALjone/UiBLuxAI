7a4b491a5442ee92913eb69b1b49637b6874a396
Audun <audun.henriksen@gmail.com>
2023-01-25 17:17:37 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from luxai_s2.env import LuxAI_S2
+import matplotlib.pyplot as plt
+import numpy as np
+from agent import Agent
+from animate import interact
+from wrappers.obs_wrappers import ImageObservationWrapper
+
+env = LuxAI_S2()
+env.reset()
+help(env.step)
+
+
+#env = ImageObservationWrapper(env)
+
+
+
+
+# recreate our agents and run
+agents = {player: Agent(player, env.state.env_cfg) for player in env.agents}
+interact(env, agents, 1000)
