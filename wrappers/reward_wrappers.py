c72cc922c5be96ba9e01768777b498ef17599961
unknown <Audun.Henriksen@gmail.com>
2023-01-30 20:59:05 +0100

    Added a way to get the obs in the reward function

--- wrappers/reward_wrappers.py
+++ wrappers/reward_wrappers.py
@@ -2,4 +2,8 @@ import gym
 
 class SimpleRewardWrapper(gym.RewardWrapper):
     def reward(self, reward):
+        obs = self.state.get_obs()
+        observations = {}
+        for k in self.agents:
+            observations[k] = obs
         return reward
