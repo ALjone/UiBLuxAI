eb3bbcd20dbd01ed4afc67d5297b2863ad5de9f7
unknown <Audun.Henriksen@gmail.com>
2023-01-31 21:04:39 +0100

    Major bug fix vol. 2

--- network/ActorCritic.py
+++ network/ActorCritic.py
@@ -26,10 +26,10 @@ class ActorCritic(nn.Module):
         unit_dist, factory_dist = Categorical(action_probs_unit), Categorical(action_probs_factories)
 
         action_unit = unit_dist.sample()
-        action_logprob_unit = unit_dist.log_prob(action_unit)*state[0]
+        action_logprob_unit = unit_dist.log_prob(action_unit)*(state[0] == 1)
 
         action_factory = factory_dist.sample()
-        action_logprob_factory = factory_dist.log_prob(action_factory)*state[0]
+        action_logprob_factory = factory_dist.log_prob(action_factory)*(state[1] == 1)
 
         state_val = self.critic(state)
 
@@ -41,11 +41,11 @@ class ActorCritic(nn.Module):
         action_probs_unit, action_probs_factories = self.actor(state)
 
         unit_dist = Categorical(action_probs_unit)
-        action_logprobs_unit = unit_dist.log_prob(unit_action)*state[:, 0]
+        action_logprobs_unit = unit_dist.log_prob(unit_action)*(state[:, 0] == 1)
         unit_dist_entropy = (unit_dist.entropy()*state[:, 0]).sum((1, 2))
 
         factory_dist = Categorical(action_probs_factories)
-        action_logprobs_factories = factory_dist.log_prob(factory_action)*state[:, 0]
+        action_logprobs_factories = factory_dist.log_prob(factory_action)*(state[:, 1] == 1)
         factory_dist_entropy = (factory_dist.entropy()*state[:, 0]).sum((1, 2))
 
         state_values = self.critic(state)
