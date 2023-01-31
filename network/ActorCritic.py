Started on action masking, fixed some minor bugs, and fixed one mega bug related to unit-action-masking

network/ActorCritic.py
@@ -19,7 +19,7 @@ class ActorCritic(nn.Module):
     def forward(self, state):
         return self.act(state)
     
-    def act(self, state):
+    def act(self, state, obs):
         #NOTE: Assumes first channel is unit mask for our agent
         #NOTE: Assumes second channel is factory mask for our agent
         action_probs_unit, action_probs_factories = self.actor(state)

ppo.py
@@ -47,11 +47,11 @@ class PPO:
         
         self.MseLoss = nn.MSELoss()
 
-    def select_action(self, state):
+    def select_action(self, state, obs):
 
         with torch.no_grad():
             state = state.float().to(self.device)
-            action_unit, action_factory, action_logprobs_unit, action_logprobs_factories, state_values = self.policy_old.act(state)
+            action_unit, action_factory, action_logprobs_unit, action_logprobs_factories, state_values = self.policy_old.act(state, obs)
         
         self.buffer.states.append(state)
         self.buffer.unit_actions.append(action_unit)

utils/action_masking.py
@@ -20,9 +20,11 @@ def calculate_move_cost(x, y, base_cost, modifier, rubble, dir):
 #5 = dig
 #6 = recharge
 #7 = self_destruct
-def single_unit_action_mask(unit, obs):
+def single_unit_action_mask(unit, obs, player = "player_0"):
     
     """Calculates the action mask for one specific unit"""
+    #TODO: Needs to take in a player for the factory stuff?
+
     #TODO: This doesn't care about the fact that adding to action queue costs 1/10 (LIGHT/HEAVY)
     #TODO: Invalid to dig on top of factory
     action_mask = torch.ones(UNIT_ACTION_IDXS)
@@ -51,8 +53,11 @@ def single_unit_action_mask(unit, obs):
     if y == 47 or power < calculate_move_cost(x, y, move_cost_base, rubble_move_modifier, rubble, "down"):
         action_mask[3] = 0
 
+    factories = obs["factories"][player]
+    factory_pos = [factory["pos"] for _, factory in factories.items()]
+
     #Dig
-    if unit["power"] < dig_cost:
+    if unit["power"] < dig_cost or list(unit["pos"]) in factory_pos:
         action_mask[5] = 0
 
     #Recharge, max is 150 for LIGHT and 3000 for heavy per config

wrappers/observation_wrappers.py
@@ -83,8 +83,11 @@ class ImageWithUnitsWrapper(gym.ObservationWrapper):
                 factory_vec[:2] /= self.env.state.env_cfg.map_size
                 factory_data[factory["pos"][0], factory["pos"][1]] = factory_vec
                 factory_mask[factory["pos"][0], factory["pos"][1]] = 1 if factory["team_id"] == 0 else 0
+
+            #NOTE: Unit mask MUST be first, factory mask MUST be second
             image_features = np.concatenate(
                 [
+                    unit_mask,
                     factory_mask,
                     factory_data,
                     np.expand_dims(shared_obs["board"]["lichen"], -1)
@@ -93,7 +96,6 @@ class ImageWithUnitsWrapper(gym.ObservationWrapper):
                     / self.env.state.env_cfg.MAX_RUBBLE,
                     np.expand_dims(shared_obs["board"]["ice"], -1),
                     np.expand_dims(shared_obs["board"]["ore"], -1),
-                    unit_mask,
                     unit_data,
                 ],
                 axis=-1,

wrappers/other_wrappers.py
@@ -30,76 +30,6 @@ class SinglePlayerEnv(gym.Wrapper):
                 return dict(spawn=spawn_loc, metal=150, water=150)
             return dict()
 
-
-
-    def get_reward(self, action, obs):
-        #NOTE: Copied from the kit
-        agent = "player_0"
-        shared_obs = obs["player_0"]
-
-        # compute reward
-        # we simply want to encourage the heavy units to move to ice tiles
-        # and mine them and then bring them back to the factory and dump it
-        # as well as survive as long as possible
-
-        factories = shared_obs["factories"][agent]
-        factory_pos = None
-        for unit_id in factories:
-            factory = factories[unit_id]
-            # note that ice converts to water at a 4:1 ratio
-            factory_pos = np.array(factory["pos"])
-            break
-        units = shared_obs["units"][agent]
-        unit_deliver_ice_reward = 0
-        unit_move_to_ice_reward = 0
-        unit_overmining_penalty = 0
-
-        ice_map = shared_obs["board"]["ice"]
-        ice_tile_locations = np.argwhere(ice_map == 1)
-
-        def manhattan_dist(p1, p2):
-            return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-
-        unit_power = 0
-        for unit_id in units:
-            unit = units[unit_id]
-            if unit["unit_type"] == "HEAVY":
-                pos = np.array(unit["pos"])
-                ice_tile_distances = np.mean((ice_tile_locations - pos) ** 2, 1)
-                closest_ice_tile = ice_tile_locations[np.argmin(ice_tile_distances)]
-                dist_to_ice = manhattan_dist(closest_ice_tile, pos)
-                unit_power = unit["power"]
-                if unit["cargo"]["ice"] < 20:
-
-                    dist_penalty = min(
-                        1.0, dist_to_ice / (10)
-                    )  # go beyond 12 squares manhattan dist and no reward
-                    unit_move_to_ice_reward += (
-                        1 - dist_penalty
-                    ) * 0.1  # encourage unit to move to ice
-                else:
-                    if factory_pos is not None:
-                        dist_to_factory = manhattan_dist(pos, factory_pos)
-                        dist_penalty = min(1.0, dist_to_factory / 10)
-                        unit_deliver_ice_reward = (
-                            0.2 + (1 - dist_penalty) * 0.1
-                        )  # encourage unit to move back to factory
-
-
-        #print(self.state.stats["player_0"]["lichen_grown"].keys())
-        #print(self.state.stats["player_0"]["units_built"]["LIGHT"])
-        reward = (
-            0
-            + unit_move_to_ice_reward
-            + unit_deliver_ice_reward
-            + unit_overmining_penalty
-            #+ (self.state.stats["player_0"]["units_built"]["LIGHT"]/(len(self.env.state.factories[agent])*15)) if len(self.agents) > 0 else 0
-        )
-        reward = reward
-
-
-        return {"player_0" : reward}
-
     def step(self, action):
         agent = self.agents[0]
         opp_agent = self.agents[1]
@@ -116,16 +46,11 @@ class SinglePlayerEnv(gym.Wrapper):
             opp_action = {}
 
         obs, reward, done, info = super().step({agent: action, opp_agent: opp_action})
-         
-        if self.state.real_env_steps < 1:
-            reward = {agent : 0}
-        else:
-            reward = self.get_reward(action, obs[1]) 
 
         self.prev_actions = action
 
         #NOTE: See here for tips on custom reward function https://github.com/Lux-AI-Challenge/Lux-Design-S2/blob/main/examples/sb3.py
-        return (obs[0][agent], obs[1]), reward[agent], done[agent], info[agent]
+        return (obs[0][agent], obs[1]), reward, done[agent], info[agent]
 
     def reset(self, **kwargs):
         obs = self.env.reset(**kwargs)

wrappers/reward_wrappers.py
@@ -34,16 +34,19 @@ class IceRewardWrapper(gym.RewardWrapper):
                 for unit_id, action in action_dict:
                     if "unit" in unit_id:
                         units += 1
-                    else:
+                    elif "factory" in unit_id:
                         factories += 1
+                    else:
+                        raise ValueError("Oh no, what's wrong with this unit ID")
                     if action[0] == 4:
                         self_destructs += 1  # Idx 4 is self destruct
 
-        units_died = units - self.number_of_units[player]
-        factories_died = factories - self.number_of_factories[player]
-        self.number_of_units[player] = units
-        self.number_of_factories[player] = factories
-        return units_died, factories_died
+            units_died = units - self.number_of_units[player] - self_destructs
+            factories_died = factories - self.number_of_factories[player]
+            self.number_of_units[player] = units
+            self.number_of_factories[player] = factories
+            return units_died, factories_died
+        return 0, 0
 
     def reward(self, rewards):
         # does not work yet. need to get the agent's observation of the current environment
@@ -59,7 +62,7 @@ class IceRewardWrapper(gym.RewardWrapper):
             lichen = self.state.board.lichen[agent_lichen_mask].sum(
             )
             # TODO: Check for correctness
-            reward = -1 if rewards < 0 else 1
+            reward = 1 if rewards["player_0"] > rewards["player_1"] else 0
             reward += np.tanh(lichen/self.config["lichen_divide_value"])*20
             return reward
 