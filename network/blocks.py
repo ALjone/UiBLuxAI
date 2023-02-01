039cc0ba86f58b54b7eca6b92b15f3fc3bd49043
SigurdSolberg <sigroll@gmail.com>
2023-02-01 17:11:24 +0100

    Added RotationalConv2D

--- network/blocks.py
+++ network/blocks.py
@@ -45,3 +45,59 @@ class ResSEBlock(nn.Module):
         x = self.SE1(x)
         x = self.activation_function(x)
         return x + inputs
+
+class RotationInvariantConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
+        super(RotationInvariantConv2d, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+        self.bias = nn.Parameter(torch.rand(out_channels), requires_grad=True).to(devise)
+        # create a parameter for each symmetric group
+        self.weights = nn.ParameterList([nn.Parameter(torch.Tensor(1, 1, 1, 1)).to(devise) for i in range(6*out_channels)]).to(device=devise)
+        self.reset_parameters()
+
+        self.weight = torch.zeros(self.out_channels, self.in_channels, self.kernel_size, self.kernel_size, device=devise)
+        for i in range(self.out_channels):
+            self.weight[i,0,0,0] = self.weights[0 + 6*i]
+            self.weight[i,0,-1,0] = self.weights[0+ 6*i]
+            self.weight[i,0,0,-1] = self.weights[0+ 6*i]
+            self.weight[i,0,-1,-1] = self.weights[0+ 6*i]
+
+            self.weight[i,0,0,2] = self.weights[1+ 6*i]
+            self.weight[i,0,2,0] = self.weights[1+ 6*i]
+            self.weight[i,0,2,-1] = self.weights[1+ 6*i]
+            self.weight[i,0,-1,2] = self.weights[1+ 6*i]
+
+            self.weight[i,0,0,1] = self.weights[2+ 6*i]
+            self.weight[i,0,0,3] = self.weights[2+ 6*i]
+            self.weight[i,0,1,0] = self.weights[2+ 6*i]
+            self.weight[i,0,3,0] = self.weights[2+ 6*i]
+            self.weight[i,0,1,-1] = self.weights[2+ 6*i]
+            self.weight[i,0,3,-1] = self.weights[2+ 6*i]
+            self.weight[i,0,-1,1] = self.weights[2+ 6*i]
+            self.weight[i,0,-1,3] = self.weights[2+ 6*i]
+
+            self.weight[i,0,1,1] = self.weights[3+ 6*i]
+            self.weight[i,0,1,3] = self.weights[3+ 6*i]
+            self.weight[i,0,3,1] = self.weights[3+ 6*i]
+            self.weight[i,0,3,3] = self.weights[3+ 6*i]
+
+            self.weight[i,0,1,2] = self.weights[4+ 6*i]
+            self.weight[i,0,3,2] = self.weights[4+ 6*i]
+            self.weight[i,0,2,1] = self.weights[4+ 6*i]
+            self.weight[i,0,2,3] = self.weights[4+ 6*i]
+
+            self.weight[i,0, 2, 2] = self.weights[5 + 6*i]
+
+    def reset_parameters(self):
+        n = self.in_channels * 6
+        stdv = 1#1. / math.sqrt(n)
+        for weight in self.weights:
+            weight.data.normal_(-stdv, stdv)
+
+    def forward(self, input):
+        return F.conv2d(input, self.weight, stride=self.stride, padding=self.padding, bias = self.bias)
