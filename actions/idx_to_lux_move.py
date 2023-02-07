fe49ed08ed7ad1736cdfd032b6037aff89a4466d
Herman Mostein <herman@visito.no>
2023-02-07 17:23:58 +0100

    Add functionality for new action space

--- actions/idx_to_lux_move.py
+++ actions/idx_to_lux_move.py
@@ -2,11 +2,12 @@ import numpy as np
 from .action_queues import move_north, move_south, move_east, move_west, move_single
 from .action_queues import pickup, self_destruct, dig
 from .action_queues import move_to_closest_factory_and_transport, move_to_closest_res, res_mining_loop
+from .action_queues import transfer_single, self_destruct
 
-UNIT_ACTION_IDXS = 12
+UNIT_ACTION_IDXS = 15
 FACTORY_ACTION_IDXS = 4
 MOVE_NAMES = moves = ["No action", "Stop", "Move north", "Move south", "Move east", "Move west", "Pickup power",
-                         "Closest factory ice", "Closest factory ore", "Closest ice", "Closest ore", "Digg", "Ice loop", "Ore loop"]
+                      "Closest factory ice", "Closest factory ore", "Closest ice", "Closest ore", "Digg"]
 
 # a[0] = action type
 # (0 = move, 1 = transfer X amount of R, 2 = pickup X amount of R, 3 = dig, 4 = self destruct, 5 = recharge X)
@@ -18,49 +19,40 @@ MOVE_NAMES = moves = ["No action", "Stop", "Move north", "Move south", "Move eas
 # a[3] = X, amount of resources transferred or picked up if action is transfer or pickup.
 # If action is recharge, it is how much energy to store before executing the next action in queue
 
-# a[4] = repeat. If repeat == 0, then action is not recycled and removed once we have executed it a[5] = n times. 
+# a[4] = repeat. If repeat == 0, then action is not recycled and removed once we have executed it a[5] = n times.
 # Otherwise if repeat > 0 we recycle this action to the back of the action queue and set n = repeat.
 
 # a[5] = n, number of times to execute this action before exhausting it and removing it from the front of the action queue. Minimum is 1.
 
-def _unit_idx_to_action(idx, obs, factory_map, unit):
+
+def _unit_idx_to_action(type_idx, direction_idx, amount_idx, obs, factory_map, unit):
     """Translates an index-action (from argmax) into a Lux-valid action for units"""
-    assert 0 < idx < UNIT_ACTION_IDXS
-    assert isinstance(idx, int)
-    if idx == 0:
-        raise ValueError("This shouldn't be possible! Better to just not submit an action")
-    if idx == 1: 
-        return [move_single(0, 0, 1)] #ABORT QUEUE
-    if idx == 2:
-        return move_north()
-    if idx == 3: 
-        return move_south()
-    if idx == 4: 
-        return move_east()
-    if idx == 5:
-        return move_west()
-    if idx == 6:
-        return pickup("power", 1000)
-    if idx == 7:
-        return move_to_closest_factory_and_transport(factory_map, unit, "ice")
-    if idx == 8:
-        return move_to_closest_factory_and_transport(factory_map, unit, "ore")
-    if idx == 9:
-        return move_to_closest_res("ice", unit, obs)
-    if idx == 10:
-        return move_to_closest_res("ore", unit, obs)
-    if idx == 11:
+
+    amount = [0.25, 0.5, 0.75, 1]
+    if (type_idx == 0):  # Move
+        return [move_single(direction_idx, 1, 1)]
+
+    elif (type_idx == 1):
+        resources = ['power', 'ice', 'water', 'ore', 'metal']
+        res = resources[direction_idx]
+        cargo = unit["cargo"][res]
+        return pickup(res, amount[amount_idx]*int(cargo))
+
+    elif (type_idx == 2):
         return dig(unit, 1, 0)
-    #TODO: THESE ARE EFFECTIVELY REMOVED DUE TO UNIT_ACTION_IDXS BEING LOWERED
-    if idx == 12:
-        return res_mining_loop("ice", unit, obs, factory_map)
-    if idx == 13:
-        return res_mining_loop("ore", unit, obs, factory_map)
+
+    # Using amount idx to indicate direction of transferal, and will always transfer all.
+    elif (type_idx == 3):
+        return transfer_single(res, 1000, direction=amount_idx)
+
+    elif (type_idx == 4):
+        return self_destruct()
 
 
 def _factory_idx_to_action(idx):
     """Translates an index-action (from argmax) into a Lux-valid action for factories"""
-    assert 0 <= idx < (FACTORY_ACTION_IDXS-1) #Minus 1 because action 3 is do nothing
+    assert 0 <= idx < (FACTORY_ACTION_IDXS -
+                       1)  # Minus 1 because action 3 is do nothing
     assert isinstance(idx, int)
     # 0: Build light, 1: Build heavy, 2: Grow lichen
     return idx
@@ -68,7 +60,8 @@ def _factory_idx_to_action(idx):
 
 def outputs_to_actions(unit_output, factory_output, units, factories, obs, factory_map):
     """Turns outputs from the model into action dicts"""
-    unit_actions = _unit_output_to_actions(unit_output, units, obs, factory_map)
+    unit_actions = _unit_output_to_actions(
+        unit_output, units, obs, factory_map)
     unit_actions.update(_factory_output_to_actions(factory_output, factories))
     return unit_actions
 
@@ -78,32 +71,37 @@ def unit_id_to_action_idx(units, unit_output):
     for unit in units:
         x, y = unit["pos"][0], unit["pos"][1]
         action_idx = unit_output[x, y].item()
-        if action_idx == 0: continue #The "Do nothing" action
+        if action_idx == 0:
+            continue  # The "Do nothing" action
         actions[unit["unit_id"]] = action_idx
 
     return actions
+
 
 def _unit_output_to_actions(unit_output, units, obs, factory_map):
     """Turns outputs from the model into action dicts for units"""
     actions = {}
     for unit in units:
-            x, y = unit["pos"][0], unit["pos"][1]
-            action = unit_output[x, y].item()
-            if action == 0: continue #The "Do nothing" action
-            action = _unit_idx_to_action(action, obs, factory_map, unit)
-            if action == []: #If you stand on ice and move to ice...
-                continue
-            actions[unit["unit_id"]] = action
+        x, y = unit["pos"][0], unit["pos"][1]
+        action = unit_output[x, y].item()
+        if action == 0:
+            continue  # The "Do nothing" action
+        action = _unit_idx_to_action(action, obs, factory_map, unit)
+        if action == []:  # If you stand on ice and move to ice...
+            continue
+        actions[unit["unit_id"]] = action
 
     return actions
+
 
 def _factory_output_to_actions(factory_output, factories):
     """Turns outputs from the model into action dicts for factories"""
     actions = {}
     for factory in factories:
-            x, y = factory["pos"][0], factory["pos"][1]
-            action = factory_output[x, y].item()
-            if action == 3: continue
-            actions[factory["unit_id"]] = _factory_idx_to_action(action)
+        x, y = factory["pos"][0], factory["pos"][1]
+        action = factory_output[x, y].item()
+        if action == 3:
+            continue
+        actions[factory["unit_id"]] = _factory_idx_to_action(action)
 
     return actions
