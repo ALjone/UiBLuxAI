Fixed logprob

network/ActorCritic.py
@@ -40,17 +40,27 @@ class ActorCritic(nn.Module):
 
         #action_probs_unit *= unit_mask
         action_probs_factories *= factory_mask
-        unit_dist, factory_dist = Categorical(action_probs_unit), Categorical(action_probs_factories)
+        type_dist, direction_dist, value_dist, factory_dist = Categorical(action_type_probs), Categorical(action_direction_probs), Categorical(action_values_probs), Categorical(action_probs_factories)
 
-        action_unit = unit_dist.sample()
-        action_logprob_unit = unit_dist.log_prob(action_unit)*(image_features[0] == 1)
+        action_type = type_dist.sample()
+        action_direction = direction_dist.sample()
+        action_value = value_dist.sample()
+
+        unit_mask = (image_features[0] == 1)
+        action_type_logprob = type_dist.log_prob(action_type)*unit_mask
+        action_direction_logprob = direction_dist.log_prob(action_direction)*unit_mask
+        action_value_logprob = value_dist.log_prob(action_value)*unit_mask
+
+        # Current problem: How do we handle logprobs when we are sampling 3 values from 3 different (not independent) distributions
+        action_logprob_unit = torch.sum(action_type_logprob.detach()) * torch.sum(action_direction_logprob.detach()) * torch.sum(action_value_logprob.detach())
 
         action_factory = factory_dist.sample()
         action_logprob_factory = factory_dist.log_prob(action_factory)*(image_features[1] == 1)
 
         state_val = self.critic(image_features, global_features)
 
-        return action_unit.detach(), action_factory.detach(), torch.sum(action_logprob_unit.detach()), torch.sum(action_logprob_factory.detach()), state_val.detach()
+        return action_type.detach(), action_direction.detach(), action_value.detach(), \
+            action_factory.detach(), torch.sum(action_logprob_unit.detach()), torch.sum(action_logprob_factory.detach()), state_val.detach()
     
     def evaluate(self, image_features, global_features, unit_action, factory_action):
         #TODO: Does this also need the same type of action masking? Yes, according to gridnet